e00c05854f82b9e8b6ec20608bf0613cce66fe16
小葛 <xiaoge1995@users.noreply.github.com>
2018-07-05 15:23:27 +0800

    Update 8.h

--- 8.h
+++ 8.h
@@ -1,15 +1,3 @@
-HintsSubmissionsDiscussSolution
-      
-C++ 4line code
-C++ 4line code
-56
-VIEWS
-0
-Created at: July 1, 2018 9:54 PM
-
-zhi-hong
-zhi-hong
- 0
 int myAtoi(string str) {
     long long ans = atoll(str.c_str());
     if (ans>INT_MAX) return INT_MAX;
