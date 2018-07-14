9e8691f20c030eb28981fe813cc9ec0d0c68c07b
小葛 <xiaoge1995@users.noreply.github.com>
2018-07-14 10:32:37 +0800

    E12

---
+++ 56.h
@@ -0,0 +1,12 @@
+vector<Interval> merge(vector<Interval>& ins) {
+    if (ins.empty()) return vector<Interval>{};
+    vector<Interval> res;
+    sort(ins.begin(), ins.end(), [](Interval a, Interval b){return a.start < b.start;});
+    res.push_back(ins[0]);
+    for (int i = 1; i < ins.size(); i++) {
+        if (res.back().end < ins[i].start) res.push_back(ins[i]);
+        else
+            res.back().end = max(res.back().end, ins[i].end);
+    }
+    return res;
+}
